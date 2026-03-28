f7033bec1e69ac56ca87c99dc0c178a3807b25ec
Nasywa Azzah Nabila <nasywaazzahnabila@gmail.com>
2026-03-28 23:03:15 +0700

    Update Beranda.py

--- Beranda.py
+++ Beranda.py
@@ -27,14 +27,18 @@ st.markdown('<div class="title">APLIKASI KLASTERISASI WILAYAH TERDAMPAK BANJIR D
 st.markdown('<hr class="divider">', unsafe_allow_html=True)
 
 # Deskripsi
-st.write("""
-Aplikasi ini digunakan untuk mengelompokkan provinsi di Indonesia menggunakan metode HDSBCAN
-(Hierarchical Density-Based Spatial Clustering of Applications with Noise) dan Bayesian Optimization
-""")
+st.markdown("""
+<div align="justify">
+Aplikasi ini digunakan untuk mengelompokkan provinsi di Indonesia menggunakan metode 
+HDBSCAN (*Hierarchical Density-Based Spatial Clustering of Applications with Noise*) 
+dan *Bayesian Optimization*.
+</div>
+""", unsafe_allow_html=True)
 
 # Latar Belakang
 st.subheader("Latar Belakang")
 st.markdown("""
+<div align="justify">
 Banjir merupakan salah satu bencana alam yang paling sering terjadi di Indonesia dengan ribuan kejadian setiap tahunnya
 yang menimbulkan dampak signifikan terhadap kerusakan infrastruktur dan gangguan aktivitas sosial masyarakat. Pada tahun 2024,
 banjir tercatat sebagai bencana alam yang paling dominan terjadi di Indonesia, disertai peningkatan jumlah korban dan kerusakan
@@ -44,15 +48,18 @@ dilakukan secara optimal dan membutuhkan pendekatan yang lebih efektif berbasis 
 Di sisi lain, karakteristik dampak banjir di setiap provinsi di Indonesia cenderung berbeda-beda karena dipengaruhi oleh kondisi
 geografis, lingkungan, maupun sosial ekonomi. Oleh karena itu, diperlukan suatu sistem yang mampu mengidentifikasi pola dampak banjir
 dan mengelompokkan provinsi di Indonesia berdasarkan tingkat keparahan dampaknya. Aplikasi ini dikembangkan dengan memanfaatkan metode
-HDBSCAN yang dikombinasikan dengan Bayesian Optimization untuk menghasilkan klasterisasi yang lebih akurat. Aplikasi ini mampu menyajikan
+HDBSCAN yang dikombinasikan dengan *Bayesian Optimization* untuk menghasilkan klasterisasi yang lebih akurat. Aplikasi ini mampu menyajikan
 hasil klasterisasi dalam bentuk visualisasi interaktif sehingga memudahkan pengguna dalam memahami distribusi tingkat keparahan dampak banjir
 serta mendukung pengambilan keputusan dalam upaya mitigasi bencana secara lebih tepat sasaran.
-""")
+</div>
+""", unsafe_allow_html=True)
 
 # Tujuan
 st.subheader("Tujuan")
 st.markdown("""
-- Mengelompokkan provinsi di Indonesia berdasarkan indikator dampak banjir menggunakan metode HDBSCAN dan Bayesian Optimization
+<div align="justify">
+- Mengelompokkan provinsi di Indonesia berdasarkan indikator dampak banjir menggunakan metode HDBSCAN dan *Bayesian Optimization*  
 - Menyediakan peta interaktif untuk menampilkan visualisasi hasil klasterisasi  
-- Menyediakan fitur  prediksi kategori dampak banjir berdasarkan input data baru dari pengguna  
-""")
+- Menyediakan fitur prediksi kategori dampak banjir berdasarkan input data baru dari pengguna  
+</div>
+""", unsafe_allow_html=True)
