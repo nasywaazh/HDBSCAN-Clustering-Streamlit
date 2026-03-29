a01e56c693b291f13c50594032dcf3221d02eab8
Nasywa Azzah Nabila <nasywaazzahnabila@gmail.com>
2026-03-29 19:21:54 +0700

    Update 1_Metode.py

--- pages/1_Metode.py
+++ pages/1_Metode.py
@@ -1,10 +1,10 @@
 import streamlit as st
 
-st.title("📘 Metode")
+st.title("Metode Klasterisasi")
 
 st.header("HDBSCAN")
 st.write("""
-HDBSCAN adalah metode clustering berbasis densitas yang mampu mengelompokkan data 
+HDBSCAN (<i>Hierarchical Density-Based Spatial Clustering of Applications with Noise</i>) adalah metode clustering berbasis densitas yang mampu mengelompokkan data 
 berdasarkan kepadatan serta mendeteksi outlier.
 """)
 
@@ -12,13 +12,4 @@ st.header("Bayesian Optimization")
 st.write("""
 Bayesian Optimization digunakan untuk menemukan parameter terbaik 
 dalam proses clustering.
-""")
-
-st.header("Alur Penelitian")
-st.write("""
-1. Pengumpulan data  
-2. Preprocessing data  
-3. Optimasi parameter  
-4. Klasterisasi HDBSCAN  
-5. Evaluasi hasil  
 """)
