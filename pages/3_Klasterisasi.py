697c1da6985728943ec80cb91e2d9dbb33c58f93
Nasywa Azzah Nabila <nasywaazzahnabila@gmail.com>
2026-03-30 15:11:05 +0700

    Update 3_Klasterisasi.py

--- pages/3_Klasterisasi.py
+++ pages/3_Klasterisasi.py
@@ -17,51 +17,71 @@ if "data" not in st.session_state:
 
 df = st.session_state["data"]
 
-# Standarisasi data
+# Preprocessing data
 st.subheader("Preprocessing Data")
-data_numeric = df.drop(columns=["Provinsi"])
-scaler = StandardScaler()
-scaled_standard = pd.DataFrame(
-    scaler.fit_transform(data_numeric),
-    columns=data_numeric.columns)
+tab1, tab2, tab3, tab4 = st.tabs([
+    "Standarisasi Data",
+    "Uji Statistik",
+    "Deteksi Outlier (LOF)",
+    "Reduksi Data (PCA)"
+])
 
-with st.expander("Hasil Standarisasi Data"):
+# Standarisasi data
+with tab1:
+    st.subheader("Standarisasi Data")
+    data_numeric = df.drop(columns=["Provinsi"])
+    scaler = StandardScaler()
+    scaled_standard = pd.DataFrame(
+        scaler.fit_transform(data_numeric),
+        columns=data_numeric.columns
+    )
     st.dataframe(scaled_standard)
 
 # Uji statistik
-st.subheader("Uji Statistik")
-kmo_all, kmo_model = calculate_kmo(scaled_standard)
-chi_square_value, p_value = calculate_bartlett_sphericity(scaled_standard)
-col1, col2 = st.columns(2)
+with tab2:
+    st.subheader("Uji Statistik")
+    # KMO & Bartlett
+    kmo_all, kmo_model = calculate_kmo(scaled_standard)
+    chi_square_value, p_value = calculate_bartlett_sphericity(scaled_standard)
+    col1, col2 = st.columns(2)
 
-## KMO
-with col1:
-    st.metric("Nilai KMO:", f"{kmo_model:.4f}")
+    with col1:
+        st.metric("Nilai KMO", f"{kmo_model:.4f}")
+    with col2:
+        st.metric("Bartlett p-value", f"{p_value:.6f}")
+        
     if kmo_model > 0.5:
         st.success("Data sudah representatif")
     else:
-        st.error("Data tidak representatif")
+        st.error("Data belum representatif")
 
-## Bartlett
-with col2:
-    st.metric("Bartlett p-value:", f"{p_value:.6f}")
     if p_value < 0.05:
-        st.success("Terdapat korelasi yang signifikan antarvariabel")
+        st.success("Terdapat korelasi signifikan antar variabel")
+        korelasi_ok = True
     else:
-        st.error("Tidak ada korelasi yang signifikan antarvariabel")
+        st.warning("Tidak ada korelasi signifikan")
+        korelasi_ok = False
 
-## Multikolinieritas
-X_vif = scaled_standard.copy()
-vif_data = pd.DataFrame()
-vif_data["Features"] = X_vif.columns
-vif_data["VIF"] = [
-    variance_inflation_factor(X_vif.values, i)
-    for i in range(X_vif.shape[1])
-]
+    # VIF
+    X_vif = scaled_standard.copy()
+    vif_data = pd.DataFrame()
+    vif_data["Variabel"] = X_vif.columns
+    vif_data["VIF"] = [
+        variance_inflation_factor(X_vif.values, i)
+        for i in range(X_vif.shape[1])
+    ]
 
-with st.expander("Uji Multikolinieritas (VIF)"):
+    st.write("Hasil Uji Multikolinieritas (VIF):")
     st.dataframe(vif_data)
-    if (vif_data["VIF"] > 5).any():
-        st.warning("Terdapat multikolinieritas (VIF > 5)")
+
+    high_vif = vif_data[vif_data["VIF"] >= 5]
+    if not high_vif.empty:
+        variabels = ", ".join(high_vif["Variabel"].tolist())
+        st.warning(
+            f"Variabel {variabels} memiliki nilai VIF ≥ 5 "
+            "yang mengindikasikan adanya multikolinieritas yang tinggi."
+        )
+        multikolinieritas = True
     else:
-        st.success("Tidak ada multikolinieritas tinggi")
+        st.success("Tidak terdapat multikolinieritas tinggi (VIF < 5)")
+        multikolinieritas = False
