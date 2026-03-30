6a7daf2b44b655e4687fb2ac345c84bb7229b2d9
Nasywa Azzah Nabila <nasywaazzahnabila@gmail.com>
2026-03-30 23:20:35 +0700

    Update 3_Klasterisasi.py

--- pages/3_Klasterisasi.py
+++ pages/3_Klasterisasi.py
@@ -444,8 +444,9 @@ with menu[2]:
         pct_row = cluster_pct_all.loc[cluster].to_frame(name="Rata-Rata Persentase (%)").T
         combined = pd.concat([mean_row, pct_row], axis=0)
         combined.index = ["Nilai Rata-rata", "Rata-Rata Persentase (%)"]
+        st.markdown(f"**Karakteristik:**")
         st.dataframe(combined)
-        st.markdown(f"**Anggota Klaster:** {n_anggota} Provinsi")
+        st.markdown(f"**Anggota Klaster:** {len(df_noise)} Provinsi")
         st.divider()
 
     df_noise = df_result[df_result["Cluster"] == -1].reset_index(drop=True)
@@ -457,5 +458,6 @@ with menu[2]:
         ) * 100).round(2).to_frame(name="Persentase (%)").T
         combined_noise = pd.concat([mean_noise, pct_noise], axis=0)
         combined_noise.index = ["Nilai Rata-rata", "Rata-Rata Persentase (%)"]
+        st.markdown(f"**Karakteristik:**")
         st.dataframe(combined_noise)
         st.markdown(f"**Anggota Klaster:** {len(df_noise)} Provinsi")
