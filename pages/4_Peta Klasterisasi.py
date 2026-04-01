f277554869a0e2f25b789dddd4a01b4e0df805a0
Nasywa Azzah Nabila <nasywaazzahnabila@gmail.com>
2026-04-01 18:01:25 +0700

    Update 4_Peta Klasterisasi.py

--- pages/4_Peta Klasterisasi.py
+++ pages/4_Peta Klasterisasi.py
@@ -6,28 +6,37 @@ import requests
 st.title("Peta Klasterisasi")
 
 # =========================
-# AMBIL DATA DARI SESSION
+# DATA
 # =========================
 if "df_clustered" not in st.session_state:
     st.warning("Silakan lakukan klasterisasi terlebih dahulu!")
     st.stop()
 
-df = st.session_state["df_clustered"]   # ✅ HARUS ADA DULU
+df = st.session_state["df_clustered"]
 
 # =========================
-# CLEAN DATA
+# CLEANING
 # =========================
 df["Provinsi"] = df["Provinsi"].str.upper().str.strip()
-df["Cluster"] = df["Cluster"].astype(str)   # ✅ BARU BOLEH DI SINI
+
+mapping = {
+    "DKI JAKARTA": "DAERAH KHUSUS IBUKOTA JAKARTA",
+    "DI YOGYAKARTA": "DAERAH ISTIMEWA YOGYAKARTA",
+    "KEP. BANGKA BELITUNG": "KEPULAUAN BANGKA BELITUNG",
+    "KEP. RIAU": "KEPULAUAN RIAU"
+}
+
+df["Provinsi"] = df["Provinsi"].replace(mapping)
+df["Cluster"] = df["Cluster"].astype(str)
 
 # =========================
-# LOAD GEOJSON
+# GEOJSON
 # =========================
 url = "https://raw.githubusercontent.com/ans-4175/peta-indonesia-geojson/master/indonesia-prov.geojson"
 geojson = requests.get(url).json()
 
 # =========================
-# WARNA KLASTER
+# WARNA
 # =========================
 color_map = {
     "0": "#66c2a5",
@@ -49,6 +58,9 @@ fig = px.choropleth(
     hover_name="Provinsi"
 )
 
-fig.update_geos(fitbounds="locations", visible=False)
+fig.update_geos(
+    fitbounds="locations",
+    visible=False
+)
 
-st.plotly_chart(fig)
+st.plotly_chart(fig, use_container_width=True)
